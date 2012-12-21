6a2bce6fc61df5a5f7b540ec115a4c2f6c61f61b
Benjamin Piwowarski <benjamin@bpiwowar.net>
2012-12-21 10:30:06 +0100

    Improved Papers2 support

--- bibtex.py
+++ bibtex.py
@@ -4,27 +4,30 @@
 # Library to convert entries into bibtex entries
 
 TYPES = {
-    "journal article": "article"
+    "journal article": "article",
+    "proceedings paper": "inproceedings"
 }
 
+
 def print_key(entry, key):
-    if entry.has_key(key):
+    if key in entry:
         print "  %s = {{%s}}" % (key, entry[key])
 
-def to_bibtex(entry):
+
+def to_bibtex(entry, crossref, short=True, crossrefs=False):
     print entry
     entryType = TYPES.get(entry["type"].lower(), "misc")
-    
-    print "%% [%s] %s" % ( entry["citekey"], entry["type"])
-    print "@%s{%s," % (entryType, entry["citekey"]) 
-    
-    author_list = " and ".join("%s, %s" % (author[0], author[1]) for author in entry.get("authors", []))
+
+    print "%% [%s] %s" % (entry["citekey"], entry["type"])
+    print "@%s{%s," % (entryType, entry["citekey"])
+
+    author_list = " and ".join("%s, %s" % (author["surname"], author["firstname"]) for author in entry.get("authors", []))
     print "  author = {%s}," % author_list
-    
+
     print_key(entry, "year")
     print_key(entry, "title")
     print_key(entry, "booktitle")
-    
+
     print "}"
     print
     print entry
