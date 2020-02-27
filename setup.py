1dacd93e1854dc3c74521e8e222975a5b8cea484
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-02-27 16:33:38 +0100

    Version 0.1 (corrections)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="biblioruler",
@@ -8,6 +8,7 @@ setup(
     author="Benjamin Piwowarski",
     author_email="benjamin@bpiwowar.net",
     url="https://github.com/bpiwowar/biblioruler",
+    packages=find_packages(),
     install_requires=["beautifulsoup4", "sqlalchemy"],
     setup_requires=["setuptools >=30.3", "setuptools_scm"],
     use_scm_version=True,
