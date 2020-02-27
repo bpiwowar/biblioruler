3451ce32dfbe08e925ee5f945f935575536d5453
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-02-27 16:30:18 +0100

    Version 0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="biblioruler",
-    version="0.1",
     description="API access to bibliographic managers",
     author="Benjamin Piwowarski",
     author_email="benjamin@bpiwowar.net",
     url="https://github.com/bpiwowar/biblioruler",
     install_requires=["beautifulsoup4", "sqlalchemy"],
+    setup_requires=["setuptools >=30.3", "setuptools_scm"],
+    use_scm_version=True,
 )
